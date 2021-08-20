a6e0c09efd4bf76d230f752f3ff2a7f0920de7da
Shay Kreymer <56560602+krshay@users.noreply.github.com>
2021-08-20 19:31:25 +0300

    Update calcBispectrum.m

--- calcBispectrum.m
+++ calcBispectrum.m
@@ -2,7 +2,7 @@ function B = calcBispectrum(x, k1k2k3_map)
 %calcBispectrum Calculates the Bispectrum; eq. (1.1)
 
 %   x is a complex N*1 vector of Fourier coefficients
-%   k1k2k3_map is a N*N matrix containing the triplets of frequencies
+%   k1k2k3_map is a N^2*3 matrix containing the triplets of frequencies
 
 %   B = x[k1]x[k2]x[-k1-k2]
 
