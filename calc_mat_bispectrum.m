72a0a04fe5ca70cd1c89f8f5dff297d7122177e9
Shay Kreymer <56560602+krshay@users.noreply.github.com>
2021-08-20 19:32:35 +0300

    Update calc_mat_bispectrum.m

--- calc_mat_bispectrum.m
+++ calc_mat_bispectrum.m
@@ -2,6 +2,10 @@ function A = calc_mat_bispectrum(K, N, k1k2k3_map)
 %calc_mat_bispectrum Calculates the sensing matrix that consists
 %of bispectra, for experiment 3a
 
+%   K is the number of samples
+%   N is the length of the signal
+%   k1k2k3_map is a N^2*3 matrix containing the triplet of frequencies
+
 A = zeros(K, N^2);
 for k=1:K
     sig = randn(N, 1) + 1j * randn(N, 1);
